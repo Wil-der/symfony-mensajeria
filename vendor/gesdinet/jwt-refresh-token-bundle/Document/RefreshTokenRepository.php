3d0adc3082e80f75848904b235b3b8cb2a7a59de
Wil-der <navarrowilder425@gmail.com>
2026-06-16 13:32:11 -0400

    arreglo de archivos de configuracion

---
+++ vendor/gesdinet/jwt-refresh-token-bundle/Document/RefreshTokenRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Gesdinet\JWTRefreshTokenBundle\Document;
+
+use DateTimeInterface;
+use DateTime;
+use Doctrine\ODM\MongoDB\Repository\DocumentRepository;
+use Gesdinet\JWTRefreshTokenBundle\Doctrine\RefreshTokenRepositoryInterface;
+
+/**
+ * @extends DocumentRepository<RefreshToken>
+ *
+ * @implements RefreshTokenRepositoryInterface<RefreshToken>
+ */
+class RefreshTokenRepository extends DocumentRepository implements RefreshTokenRepositoryInterface
+{
+    /**
+     * @param DateTimeInterface|null $datetime
+     *
+     * @return RefreshToken[]
+     */
+    public function findInvalid($datetime = null)
+    {
+        $datetime = (null === $datetime) ? new DateTime() : $datetime;
+
+        $queryBuilder = $this->createQueryBuilder()
+            ->field('valid')->lt($datetime);
+
+        return $queryBuilder->getQuery()->execute();
+    }
+}
